ec8c8a9b2238ec28e6209c4be71200a269d2868d
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-07 08:59:57 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'theme/app_theme.dart';
+import 'app.dart';
 
+void main() {
+  runApp(const MyKasApp());
+}
+
+class MyKasApp extends StatelessWidget {
+  const MyKasApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyKas',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system, // Otomatis menyesuaikan perangkat
+      home: const MainAppScreen(),
+    );
+  }
+}
