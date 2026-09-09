b77b54dfdf50e85923020b7a0408bb8238e1098d
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-09 08:32:03 +0700

    Update transaksi_model.dart

--- lib/models/transaksi_model.dart
+++ lib/models/transaksi_model.dart
@@ -1,7 +1,7 @@
 class TransaksiModel {
-  final String id; // Nanti digenerate atau dari GAS
+  final String id;
   final String tanggal;
-  final String jenis; // "Pemasukan" / "Pengeluaran"
+  final String jenis;
   final double nominal;
   final String keterangan;
   final String kategori;
@@ -17,20 +17,18 @@ class TransaksiModel {
     required this.dompet,
   });
 
-  // Fungsi untuk nerima JSON dari Google Apps Script
   factory TransaksiModel.fromJson(Map<String, dynamic> json) {
     return TransaksiModel(
       id: json['id'] ?? '',
       tanggal: json['tanggal'] ?? '',
       jenis: json['jenis'] ?? '',
-      nominal: double.parse(json['nominal'].toString()),
+      nominal: double.tryParse(json['nominal'].toString()) ?? 0.0,
       keterangan: json['keterangan'] ?? '',
       kategori: json['kategori'] ?? '',
       dompet: json['dompet'] ?? '',
     );
   }
 
-  // Fungsi untuk ngirim JSON ke Google Apps Script
   Map<String, dynamic> toJson() {
     return {
       'tanggal': tanggal,
