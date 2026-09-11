9f344b22e7e5bfdd81e8b77390545dbaaa369fb4
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-11 23:07:39 +0700

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -3,8 +3,8 @@ import 'screens/beranda/beranda_screen.dart';
 import 'screens/analisis/analisis_screen.dart';
 import 'screens/dompet/dompet_screen.dart';
 import 'screens/profil/profil_screen.dart';
-import 'widgets/bottom_nav_bar.dart';
-import 'widgets/cta_bottom_sheet.dart';
+import 'widgets/mk_bottom_nav_bar.dart';
+import 'widgets/add_transaction_bottom_sheet.dart';
 
 class MainAppScreen extends StatefulWidget {
   const MainAppScreen({super.key});
@@ -16,16 +16,17 @@ class MainAppScreen extends StatefulWidget {
 class _MainAppScreenState extends State<MainAppScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _screens = [
-    const BerandaScreen(),
-    const AnalisisScreen(),
-    const DompetScreen(),
-    const ProfilScreen(),
+  final List<Widget> _screens = const [
+    BerandaScreen(),
+    AnalisisScreen(),
+    DompetScreen(),
+    ProfilScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true, // Memungkinkan konten melayang di bawah floating bottom navbar
       body: IndexedStack(
         index: _currentIndex,
         children: _screens,
