c9f815ca11f89dcbb304f9b6e12ad146304eef3e
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-22 08:39:36 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,23 @@ void main() {
   runApp(const MyKasApp());
 }
 
-class MyKasApp extends StatelessWidget {
+class MyKasApp extends StatefulWidget {
   const MyKasApp({super.key});
+
+  @override
+  State<MyKasApp> createState() => _MyKasAppState();
+}
+
+class _MyKasAppState extends State<MyKasApp> {
+  // 1. Variabel penampung state tema global
+  ThemeMode _themeMode = ThemeMode.dark; // Default Dark Mode
+
+  // 2. Fungsi pengubah tema yang akan dipanggil dari ProfilScreen
+  void _toggleTheme(bool isDark) {
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +32,10 @@ class MyKasApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      home: const App(),
+      themeMode: _themeMode, // 3. Mengontrol tema aplikasi
+      home: App(
+        onThemeChanged: _toggleTheme, // 4. Kirim fungsi ke App
+      ),
     );
   }
 }
