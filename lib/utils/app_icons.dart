d8cc1c26bc6b0b2624a85e84a403c05a62786c06
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-23 12:02:53 +0700

    Refactor icon definitions into AppIcons class
    Refactored icon definitions by consolidating them into the AppIcons class, utilizing LucideIcons and FontAwesomeIcons for better organization and maintainability.

--- lib/utils/app_icons.dart
+++ lib/utils/app_icons.dart
@@ -1,59 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
-/// Single Source of Truth Icon Adapter MyKas
-/// Menggunakan Material Icons Bawaan Flutter (Zero External Dependency & Dart 3 Safe)
-class LucideIcons {
-  static const IconData house = Icons.home_rounded;
-  static const IconData layoutGrid = Icons.grid_view_rounded;
-  static const IconData barChart3 = Icons.bar_chart_rounded;
-  static const IconData folderOpen = Icons.folder_open_rounded;
-  static const IconData wallet = Icons.account_balance_wallet_rounded;
-  static const IconData history = Icons.history_rounded;
-  static const IconData user = Icons.person_rounded;
-  static const IconData x = Icons.close_rounded;
-  static const IconData pencil = Icons.edit_rounded;
-  static const IconData sliders = Icons.tune_rounded;
-  static const IconData checkCircle2 = Icons.check_circle_rounded;
-  static const IconData trash2 = Icons.delete_outline_rounded;
-  static const IconData chevronRight = Icons.chevron_right_rounded;
-  static const IconData utensils = Icons.restaurant_rounded;
-  static const IconData shoppingBag = Icons.shopping_bag_rounded;
-  static const IconData shoppingCart = Icons.shopping_cart_rounded;
-  static const IconData arrowUpRight = Icons.north_east_rounded;
-  static const IconData arrowDownLeft = Icons.south_west_rounded;
-  static const IconData clock = Icons.access_time_rounded;
-  static const IconData eye = Icons.visibility_rounded;
-  static const IconData eyeOff = Icons.visibility_off_rounded;
-  static const IconData plus = Icons.add_rounded;
-  static const IconData qrCode = Icons.qr_code_scanner_rounded;
-  static const IconData arrowLeftRight = Icons.swap_horiz_rounded;
-  static const IconData repeat = Icons.repeat_rounded;
-  static const IconData target = Icons.track_changes_rounded;
-  static const IconData sparkles = Icons.auto_awesome_rounded;
-  static const IconData receipt = Icons.receipt_long_rounded;
-  static const IconData arrowLeft = Icons.arrow_back_rounded;
-  static const IconData moreVertical = Icons.more_vert_rounded;
-  static const IconData bell = Icons.notifications_rounded;
-  static const IconData car = Icons.directions_car_rounded;
-  static const IconData zap = Icons.bolt_rounded;
-  static const IconData search = Icons.search_rounded;
-  static const IconData listFilter = Icons.filter_list_rounded;
-  static const IconData lock = Icons.lock_outline_rounded;
-  static const IconData fingerprint = Icons.fingerprint_rounded;
-  static const IconData smartphone = Icons.smartphone_rounded;
-  static const IconData moon = Icons.dark_mode_rounded;
-  static const IconData sun = Icons.light_mode_rounded;
-  static const IconData fileSpreadsheet = Icons.table_chart_rounded;
-  static const IconData globe = Icons.language_rounded;
-  static const IconData helpCircle = Icons.help_outline_rounded;
-  static const IconData shieldCheck = Icons.verified_user_rounded;
-  static const IconData info = Icons.info_outline_rounded;
-}
+class AppIcons {
+  // Navigation & Layout
+  static const IconData wallet = LucideIcons.wallet;
+  static const IconData layoutGrid = LucideIcons.layoutGrid;
+  static const IconData barChart = LucideIcons.barChart3;
+  static const IconData history = LucideIcons.history;
+  static const IconData user = LucideIcons.user;
+  static const IconData bell = LucideIcons.bell;
+  static const IconData plus = FontAwesomeIcons.plus;
+  static const IconData close = LucideIcons.x;
+  static const IconData search = LucideIcons.search;
+  static const IconData filter = LucideIcons.listFilter;
+  static const IconData chevronRight = LucideIcons.chevronRight;
+  static const IconData moreVertical = LucideIcons.moreVertical;
+  static const IconData eye = LucideIcons.eye;
+  static const IconData eyeOff = LucideIcons.eyeOff;
+  static const IconData clock = LucideIcons.clock;
+  static const IconData sparkles = LucideIcons.sparkles;
+  static const IconData receipt = LucideIcons.receipt;
 
-class FontAwesomeIcons {
-  static const IconData house = Icons.home_rounded;
-  static const IconData chartPie = Icons.pie_chart_rounded;
-  static const IconData folderOpen = Icons.folder_open_rounded;
-  static const IconData user = Icons.person_rounded;
-  static const IconData plus = Icons.add_rounded;
+  // Actions & Indicators
+  static const IconData arrowDownLeft = LucideIcons.arrowDownLeft;
+  static const IconData arrowUpRight = LucideIcons.arrowUpRight;
+  static const IconData utensils = LucideIcons.utensils;
+  static const IconData shoppingBag = LucideIcons.shoppingBag;
+  static const IconData car = LucideIcons.car;
+  static const IconData zap = LucideIcons.zap;
 }
