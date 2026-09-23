6379abf1b0268e01cbd5026ec7380e84a7ac9018
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-23 12:06:18 +0700

    Update app_icons.dart

--- lib/utils/app_icons.dart
+++ lib/utils/app_icons.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 
+/// Single Source of Truth Adaptor Ikon MyKas.
+/// Mencegah breaking changes & issue "Couldn't resolve package" pada Web/Mobile.
 class AppIcons {
-  // Navigation & Layout
+  // Navigation & Primary Layout
   static const IconData wallet = LucideIcons.wallet;
   static const IconData layoutGrid = LucideIcons.layoutGrid;
   static const IconData barChart = LucideIcons.barChart3;
@@ -11,6 +13,7 @@ class AppIcons {
   static const IconData user = LucideIcons.user;
   static const IconData bell = LucideIcons.bell;
   static const IconData plus = FontAwesomeIcons.plus;
+  static const IconData x = LucideIcons.x;
   static const IconData close = LucideIcons.x;
   static const IconData search = LucideIcons.search;
   static const IconData filter = LucideIcons.listFilter;
@@ -22,11 +25,23 @@ class AppIcons {
   static const IconData sparkles = LucideIcons.sparkles;
   static const IconData receipt = LucideIcons.receipt;
 
-  // Actions & Indicators
+  // Actions & Management Tools
+  static const IconData qrCode = LucideIcons.qrCode;
+  static const IconData transfer = LucideIcons.arrowLeftRight;
+  static const IconData repeat = LucideIcons.repeat;
+  static const IconData target = LucideIcons.target;
+  static const IconData pencil = LucideIcons.pencil;
+  static const IconData sliders = LucideIcons.sliders;
+  static const IconData checkCircle2 = LucideIcons.checkCircle2;
+  static const IconData trash2 = LucideIcons.trash2;
+  static const IconData arrowLeft = LucideIcons.arrowLeft;
+
+  // Finance Indicators & Categories
   static const IconData arrowDownLeft = LucideIcons.arrowDownLeft;
   static const IconData arrowUpRight = LucideIcons.arrowUpRight;
   static const IconData utensils = LucideIcons.utensils;
   static const IconData shoppingBag = LucideIcons.shoppingBag;
+  static const IconData shoppingCart = LucideIcons.shoppingCart;
   static const IconData car = LucideIcons.car;
   static const IconData zap = LucideIcons.zap;
 }
