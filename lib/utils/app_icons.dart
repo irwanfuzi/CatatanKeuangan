0332df9463159aa811db80d2d98be6728c39ea0c
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-24 07:05:02 +0700

    Update app_icons.dart

--- lib/utils/app_icons.dart
+++ lib/utils/app_icons.dart
@@ -1,45 +1,36 @@
 import 'package:flutter/material.dart';
 
-/// Single Source of Truth Adaptor Ikon MyKas.
-/// Menggunakan Material Icons M3 bawaan SDK (100% Bebas Error Kompilasi Web/dart2js).
+/// Centralized Material 3 Icon Adapter untuk MyKas.
+/// Menjamin 100% ketersediaan ikon & mencegah crash pada kompilasi dart2js Web.
 class AppIcons {
-  // Navigasi Utama & Layout
-  static const IconData wallet = Icons.account_balance_wallet_rounded;
   static const IconData layoutGrid = Icons.grid_view_rounded;
   static const IconData barChart = Icons.bar_chart_rounded;
   static const IconData history = Icons.history_rounded;
-  static const IconData user = Icons.person_rounded;
-  static const IconData bell = Icons.notifications_rounded;
+  static const IconData user = Icons.person_outline_rounded;
   static const IconData plus = Icons.add_rounded;
-  static const IconData x = Icons.close_rounded;
-  static const IconData close = Icons.close_rounded;
-  static const IconData search = Icons.search_rounded;
-  static const IconData filter = Icons.filter_list_rounded;
-  static const IconData chevronRight = Icons.chevron_right_rounded;
-  static const IconData moreVertical = Icons.more_vert_rounded;
-  static const IconData eye = Icons.visibility_rounded;
-  static const IconData eyeOff = Icons.visibility_off_rounded;
+  static const IconData eye = Icons.visibility_outlined;
+  static const IconData eyeOff = Icons.visibility_off_outlined;
   static const IconData clock = Icons.access_time_rounded;
-  static const IconData sparkles = Icons.auto_awesome_rounded;
-  static const IconData receipt = Icons.receipt_long_rounded;
-
-  // Manajemen & Aksi Cepat
-  static const IconData qrCode = Icons.qr_code_scanner_rounded;
-  static const IconData transfer = Icons.swap_horiz_rounded;
-  static const IconData repeat = Icons.replay_rounded;
-  static const IconData target = Icons.track_changes_rounded;
-  static const IconData pencil = Icons.edit_rounded;
-  static const IconData sliders = Icons.tune_rounded;
-  static const IconData checkCircle2 = Icons.check_circle_rounded;
-  static const IconData trash2 = Icons.delete_outline_rounded;
+  static const IconData bell = Icons.notifications_none_rounded;
+  static const IconData chevronRight = Icons.chevron_right_rounded;
   static const IconData arrowLeft = Icons.arrow_back_rounded;
-
-  // Kategori Keuangan & Indikator
   static const IconData arrowDownLeft = Icons.south_west_rounded;
   static const IconData arrowUpRight = Icons.north_east_rounded;
+  static const IconData qrCode = Icons.qr_code_scanner_rounded;
+  static const IconData transfer = Icons.swap_horiz_rounded;
+  static const IconData repeat = Icons.autorenew_rounded;
+  static const IconData target = Icons.track_changes_rounded;
+  static const IconData sliders = Icons.tune_rounded;
+  static const IconData receipt = Icons.receipt_long_rounded;
+  static const IconData wallet = Icons.account_balance_wallet_outlined;
+  static const IconData search = Icons.search_rounded;
+  static const IconData sparkles = Icons.auto_awesome_rounded;
+  static const IconData pencil = Icons.edit_outlined;
+  static const IconData checkCircle2 = Icons.check_circle_outline_rounded;
+  static const IconData trash2 = Icons.delete_outline_rounded;
+  static const IconData moreVertical = Icons.more_vert_rounded;
+  static const IconData x = Icons.close_rounded;
   static const IconData utensils = Icons.restaurant_rounded;
-  static const IconData shoppingBag = Icons.local_mall_rounded;
-  static const IconData shoppingCart = Icons.shopping_cart_rounded;
-  static const IconData car = Icons.directions_car_rounded;
-  static const IconData zap = Icons.bolt_rounded;
+  static const IconData shoppingBag = Icons.shopping_bag_outlined;
+  static const IconData shoppingCart = Icons.shopping_cart_outlined;
 }
