6660aaac12fa2f9c42bf772fffc01fde0ebcd8a3
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-24 10:53:49 +0700

    Update app_icons.dart

--- lib/utils/app_icons.dart
+++ lib/utils/app_icons.dart
@@ -33,4 +33,9 @@ class AppIcons {
   static const IconData utensils = Icons.restaurant_rounded;
   static const IconData shoppingBag = Icons.shopping_bag_outlined;
   static const IconData shoppingCart = Icons.shopping_cart_outlined;
+
+  // Ikon Tambahan untuk Analisis, Profil & Riwayat Screen
+  static const IconData zap = Icons.bolt_rounded;
+  static const IconData close = Icons.close_rounded;
+  static const IconData filter = Icons.filter_list_rounded;
 }
