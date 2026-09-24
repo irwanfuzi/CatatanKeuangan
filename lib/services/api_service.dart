1f492c8d7aa932ff3643f94f057745ae6ff1f97e
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-24 12:51:18 +0700

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,33 +1,7 @@
 class ApiService {
   static Future<Map<String, dynamic>> getSummary() async {
+    // Simulasi pemanggilan API backend
     await Future.delayed(const Duration(milliseconds: 200));
-    return {
-      'saldo': 'Rp 11.250.000',
-      'pemasukan': 'Rp 5.250.000',
-      'pengeluaran': 'Rp 2.804.178',
-      'riwayat': [
-        {
-          'judul': 'Gudeg Bu Dani Solo',
-          'kategori': 'Kuliner & Makanan',
-          'tanggal': 'Hari Ini, 12:45',
-          'nominal': '45000',
-          'jenis': 'pengeluaran',
-        },
-        {
-          'judul': 'Gaji Bulanan Utama',
-          'kategori': 'Payroll Inflow',
-          'tanggal': '25 Agu 2026',
-          'nominal': '8500000',
-          'jenis': 'pemasukan',
-        },
-        {
-          'judul': 'GoFood Indonesia',
-          'kategori': 'Layanan Antar',
-          'tanggal': '24 Agu 2026',
-          'nominal': '68000',
-          'jenis': 'pengeluaran',
-        },
-      ]
-    };
+    return {};
   }
 }
