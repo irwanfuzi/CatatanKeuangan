558548935aeecb6ef6750c2065434e11ad752755
irwanfuzi <irwanfuzi23@gmail.com>
2026-09-24 12:43:48 +0700

    Update quick_action_item.dart

--- lib/models/quick_action_item.dart
+++ lib/models/quick_action_item.dart
@@ -16,7 +16,7 @@ class QuickActionItem {
     this.isEnabled = false,
   });
 
-  /// 13 Fitur Lengkap Kustomisasi Quick Actions MyKas (Aman & Bebas Crash)
+  /// 13 Fitur Lengkap Kustomisasi Quick Actions MyKas
   static List<QuickActionItem> get defaultList => [
         QuickActionItem(
           id: 'scan_struk',
@@ -85,7 +85,7 @@ class QuickActionItem {
           id: 'import_data',
           title: 'Import Data',
           description: 'Masukkan mutasi bank / CSV',
-          icon: AppIcons.receipt, // FIXED: Menggantikan AppIcons.fileText yang tidak ada
+          icon: AppIcons.receipt,
           isEnabled: false,
         ),
         QuickActionItem(
